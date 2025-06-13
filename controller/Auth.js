5c1826f86d19ffb9386acb3448ed5a2a335471ae
m-s-sat <mrinalsatyarthi2005@gmail.com>
2025-06-13 16:15:56 +0530

    passport authentication integrated with backend

--- controller/Auth.js
+++ controller/Auth.js
@@ -1,26 +1,27 @@
 const { User } = require("../model/User");
+const crypto = require('crypto');
+const { sanitizeUser } = require("../services/common");
 
 exports.createUser = async(req,res)=>{
-    const user = new User(req.body);
     try{        
-        const doc = await user.save();
-        res.status(200).json({id:doc.id,role:doc.role});
+        const salt = crypto.randomBytes(16);
+        crypto.pbkdf2(req.body.password,salt,310000,32,'sha256',async function(err,hashedPassword){
+            const user = new User({...req.body, password:hashedPassword, salt});
+            const doc = await user.save();
+            req.login(sanitizeUser(doc),(err)=>{
+                if(err) res.status(400).json(err);
+                else res.status(201).json(doc);
+            });
+            res.status(200).json(sanitizeUser(doc));
+        })
     }
     catch(err){
         res.status(400).json(err);
     }
 }
 exports.loginUser = async(req,res)=>{
-    try{
-        const user = await User.findOne({email:req.body.email});
-        if(user.password===req.body.password){
-            res.status(201).json({id:user.id, role:user.role});
-        }
-        else{
-            res.status(401).json({message:'invalid credentials'});
-        }
-    }
-    catch(err){
-        res.status(400).json({message:"no such user exist"});
-    }
+    res.json(req.user);
+}
+exports.checkUser = async(req,res)=>{
+    res.json(req.user);
 }
--- index.js
+++ index.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const server = express();
 const mongoose = require('mongoose');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const express_session = require('express-session');
+const crypto = require('crypto');
 const productRouters = require('./routes/Products');
 const categoriesRouter = require('./routes/Category');
 const brandsRouter = require('./routes/Brand');
@@ -10,6 +14,8 @@ const authRouter = require('./routes/Auth');
 const cartRouter = require('./routes/Cart');
 const orderRouter = require('./routes/Order');
 const cors = require('cors');
+const { User } = require('./model/User');
+const { isAuth, sanitizeUser } = require('./services/common');
 
 const port = process.env.PORT;
 
@@ -17,13 +23,50 @@ server.use(express.json());
 server.use(cors({
     exposedHeaders:['X-Total-Count']
 }));
-server.use('/products', productRouters.router);
+server.use(express_session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+server.use(passport.authenticate('session'));
+server.use('/products',isAuth,productRouters.router);
 server.use('/category',categoriesRouter.router);
 server.use('/brands',brandsRouter.router);
 server.use('/users',usersRouter.router);
 server.use('/auth',authRouter.router);
 server.use('/cart',cartRouter.router);
 server.use('/orders',orderRouter.router);
+
+passport.use(new LocalStrategy(
+    async function (username, password, done){
+        try{
+            const user = await User.findOne({email:username});
+            if(!user) done(null, false, {message:'no such user email'});
+            crypto.pbkdf2(password,user.salt,310000,32,'sha256',async function(err,hashedPassword){
+            if(!crypto.timingSafeEqual(user.password, hashedPassword)){
+                return done(null, false, {message:'invalid credentials'});
+            }
+            done(null, sanitizeUser(user));
+        })
+    }
+    catch(err){
+        done(err);
+    }
+    }
+))
+
+// this create session variable req.user on being called from callback
+passport.serializeUser(function(user,cb){
+    process.nextTick(function(){
+        return cb(null,user);
+    });
+});
+// this creates session variable req.user when called from authorized request
+passport.deserializeUser(function(user,cb){
+    process.nextTick(function(){
+        return cb(null,{id:user.id, role:user.role});
+    });
+});
 
 async function main(){
     mongoose.connect('mongodb://127.0.0.1:27017/ecommerce-full-stack-databaase');
